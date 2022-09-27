d14bbe46521f1f4db27b977af67064858989c51a
Luke <lukedeffenbaugh@gmail.com>
2022-09-27 14:25:44 -0500

    working, uncomplete {prelab5}

--- Pre Labs/Pre Lab 5/prelab5.c
+++ Pre Labs/Pre Lab 5/prelab5.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct { 
+// ##################################################################
+
+void * createArray(int arraySize, int dataTypeSize){ 
+    int *array;
+    array = (int *)malloc(arraySize*dataTypeSize+(2*sizeof(int))); // create array of size array, type data type.
+    if(!array) return NULL;
+
+    array[0] = dataTypeSize;
+    array[1] = arraySize;   // save both size and type size
+    return (void *)(array+2); // return pointer 
+}
+
+int getArraySize(void * array){
+    int arraySize = *(((int *)array)-1);
+    return arraySize;
+}
+
+void freeArray(void * array){
+    free((int*)array-2);
+}
+
+
+// ##################################################################
+
+typedef struct employee{ // Employee Struct
     int empID, jobType; 
     float salary; 
 } Employee;
