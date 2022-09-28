6de024e979f5e5ddb2fd354e40b3998ca05234e0
Luke <lukedeffenbaugh@gmail.com>
2022-09-27 23:05:10 -0500

    working, incomplete {prelab 5}

--- Pre Labs/Pre Lab 5/prelab5.c
+++ Pre Labs/Pre Lab 5/prelab5.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// ##################################################################
+// #################### No Touch ##############################
 
 void * createArray(int arraySize, int dataTypeSize){ 
     int *array;
@@ -34,9 +34,29 @@ Employee * readEmployeeArray(FILE *fp);
 Employee * getEmployeeByID(Employee *, int empID);
 
 int main(void){
-    Employee jeff;
+    FILE *fp = fopen("empData.txt", "r");
+    if(fp == NULL){
+        printf("Could not open file\n");
+        return 0;
+    }
+    Employee *array = readEmployeeArray(fp);
+    for(int i = 0; i< *(((int *)array)-1); i++) printf("%d, %d, %f\n", array[i].empID, array[i].jobType, array[i].salary);
+
+    array[1].salary = 1234.65123;
+    printf("%f\n", array[1].salary);
+
 }
 
 Employee * readEmployeeArray(FILE *fp){
-    
+    int count = 1;  // 1 because you want the first line included
+    for(char c = getc(fp); c!= EOF; c = getc(fp)){  
+        if(c=='\n') count++;        // Counting how many lines are in the file.
+    }
+
+    rewind(fp); // Move the file pointer back to the beginning of the file.
+    Employee *array = createArray(8, sizeof(Employee));
+    for(int i = 0; i<8; i++){
+        fscanf(fp, "%d %d %f\n", &(array[i].empID), &(array[i].jobType), &(array[i].salary));
+    }
+    return array;
 }
